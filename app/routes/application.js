ed5d8c1a24cd4fda19a7545c5e30c7e06b5b3aaa
Michaeldean147 <michaeldean147@yahoo.com>
2015-02-23 17:06:21 -0800

    first styling commit

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,11 +4,13 @@ export default Ember.Route.extend({
   actions: {
     search: function(){
       var searchterm = this.controllerFor('application').get('searchWord')
+      this.controllerFor('application').set('searchWord', "")
       this.transitionTo('search', searchterm)
     },
 
     subSearch: function(){
       var searchterm = this.controllerFor('application').get('subSearchWord')
+      this.controllerFor('application').set('subSearchWord', "")
       this.transitionTo('subsearch', searchterm)
     },
   }
